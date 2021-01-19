8f8c5a79517e09093fd21167ed5754a5bbe50fe8
khyatijethva <48006079+khyatijethva@users.noreply.github.com>
2021-01-19 19:37:52 +0530

    file upload

---
+++ footer.php
@@ -0,0 +1,40 @@
+  <!-- Copyright -->
+  <div class="copyright">
+        <p>&copy; 2021 <a href="index.html">Khyati J</a>. All Rights Reserved.</p>
+    </div>
+</div>
+ <!-- Hidden Menu -->
+ <div class="additional-menu-content">
+        <h3 class="title-small">Web Developer</h3>
+        <h2 class="title">Khyati Jethava</h2> <img src="images/about.jpg" alt="">
+        <p>Hi, My name is Khyati Jethava. I am a Web Developer, and etiam accumsan scelerisque rhoncus. Nulla quis laoreet velit. Pelteqle quisu velleopha retra congue.</p> <a href="contact.php" class="btn">Hire Me</a>
+    </div>
+    <!-- My Info -->
+    <div class="my-info">
+        <div class="box">
+            <div class="field field-my-info field-separator">
+                <h6>Location</h6>
+                <p>Ahmedabad, INDIA</p>
+            </div>
+            <div class="field field-my-info field-separator">
+                <h6>E-Mail</h6>
+                <p><a href="mailto:khyatijethava35@gmail.com">khyatijethava35@gmail.com</a></p>
+            </div>
+            <div class="field field-my-info field-separator">
+                <h6>Phone</h6>
+                <p><a href="tel:+91 8980920203">+91 89 8092 0203</a></p>
+            </div>
+            <div class="clear"></div>
+            <a href="./images/04.jpg" class="button-color" download="04">
+                <span class="ti-download"></span> DOWNLOAD CV
+            </a>
+        </div>
+    </div>
+    <!-- Js -->
+    <script src="assets/js/jquery-3.3.1.min.js"></script>
+    <script src="assets/js/modernizr-custom.js"></script>
+    <script src="assets/js/owl-carousel.min.js"></script>
+    <script src="assets/js/functions.js"></script>
+</body>
+
+</html>
